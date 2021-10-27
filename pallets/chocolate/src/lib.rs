4f95a0ebb9ae6cf7a02ef962e26b3ecacf3308bf
islam-machine <76726548+islami00@users.noreply.github.com>
2021-10-27 21:26:08 +0100

    Added create reviews extrinsic.
    * Added reviewers list for checking duplicate reviews
    * Manually tested edge cases, tests could do with updates

--- pallets/chocolate/src/lib.rs
+++ pallets/chocolate/src/lib.rs
@@ -19,7 +19,7 @@ pub mod constants;
 
 #[frame_support::pallet]
 pub mod pallet {
-	use crate::constants::project;
+	use crate::constants;
 	use frame_support::{
 		dispatch::DispatchResult,
 		pallet_prelude::*,
@@ -128,6 +128,8 @@ pub mod pallet {
 	pub struct Project<UserID> {
 		/// The owner of the project
 		owner_id: UserID,
+		/// A list of the project's reviewers for validation
+		reviewers: Option<Vec<UserID>>,
 		/// A list of the project's reviews - Vec
 		reviews: Option<Vec<ReviewID>>,
 		/// A bool that allows for simple allocation of the unique chocolate badge. NFT??
@@ -149,10 +151,12 @@ pub mod pallet {
 	/// Storage map from the review index - id to the reviews
 	#[pallet::storage]
 	pub type Reviews<T: Config> = StorageMap<_, Blake2_128Concat, ReviewID, ReviewAl<T>>;
-	/// Storage value for project index. Increment as we go
+	/// Storage value for project index. Increment as we go.
+	/// Analogous to length of project map
 	#[pallet::storage]
 	pub type ProjectIndex<T: Config> = StorageValue<_, ProjectID>;
-	/// Storage value for reviews index. Increment as we go
+	/// Storage value for reviews index. Increment as we go.
+	/// Analogous to length of review map
 	#[pallet::storage]
 	pub type ReviewIndex<T: Config> = StorageValue<_, ReviewID>;
 	// The pallet's runtime storage items.
@@ -174,16 +178,23 @@ pub mod pallet {
 		SomethingStored(u32, T::AccountId),
 		/// parameters. [owner,cid]
 		ProjectCreated(T::AccountId, Vec<u8>),
+		/// parameters. [owner,project_id]
+		ReviewCreated(T::AccountId, ProjectID),
 		/// Minted [amount]
 		Minted(BalanceOf<T>),
 	}
-
 	// Errors inform users that something went wrong.
 	#[pallet::error]
 	pub enum Error<T> {
 		NoneValue,
-		/// Errors should have helpful documentation associated with them.
+		/// The project does not exist
+		NoProjectWithId,
+		/// The reviewer has already placed a review on this project with following id
+		DuplicateReview,
+		/// The index exceeds max usize.
 		StorageOverflow,
+		/// Project owners cannot review their projects
+		OwnerReviewedProject,
 	}
 
 	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
@@ -208,7 +219,6 @@ pub mod pallet {
 			// Return a successful DispatchResultWithPostInfo
 			Ok(())
 		}
-
 		/// An example dispatchable that may throw a custom error.
 		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1))]
 		pub fn cause_error(origin: OriginFor<T>) -> DispatchResult {
@@ -227,24 +237,66 @@ pub mod pallet {
 				}
 			}
 		}
-		// Refactor TO-DO: Abstract validation into a function and generalise.
 		/// Create a project
-		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(2,3))]
+		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,3))]
 		pub fn create_project(origin: OriginFor<T>, project_meta: Vec<u8>) -> DispatchResult {
 			let who = ensure_signed(origin)?;
+			// CHECKS
 			let n_index = <ProjectIndex<T>>::get().unwrap_or_default();
+			let new = n_index.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
+			// STORAGE MUTATIONS
 			<Projects<T>>::insert(
 				n_index.clone(),
 				Project {
 					owner_id: who.clone(),
+					reviewers: Option::None,
 					reviews: Option::None,
 					badge: Option::None,
 					metadata: project_meta.clone(),
 					proposal_status: Default::default(),
 				},
 			);
-			<ProjectIndex<T>>::put(n_index + 1);
+			<ProjectIndex<T>>::put(new);
 			Self::deposit_event(Event::ProjectCreated(who, project_meta));
+			Ok(())
+		}
+		/// Create a review by updating the list of reviewers and reviews of a project and adding review to storage.
+		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(2,3))]
+		pub fn create_review(
+			origin: OriginFor<T>,
+			review_meta: Vec<u8>,
+			project_id: ProjectID,
+		) -> DispatchResult {
+			let who = ensure_signed(origin)?;
+			// CHECKS
+			let n_index = <ReviewIndex<T>>::get().unwrap_or_default();
+			let new = n_index.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
+			let mut this_project =
+				<Projects<T>>::get(project_id).ok_or(Error::<T>::NoProjectWithId)?;
+			let mut list_of_reviewers = this_project.reviewers.unwrap_or_default();
+			let mut list_of_reviews = this_project.reviews.unwrap_or_default();
+			ensure!(!list_of_reviewers.contains(&who), Error::<T>::DuplicateReview);
+			ensure!(this_project.owner_id.ne(&who), Error::<T>::OwnerReviewedProject);
+			// MUTATIONS
+			// neither account ids nor the index should exceed max isize
+			list_of_reviewers.push(who.clone());
+			list_of_reviews.push(n_index.clone());
+			// STORAGE MUTATIONS
+			this_project.reviewers = Option::Some(list_of_reviewers);
+			this_project.reviews = Option::Some(list_of_reviews);
+			<Reviews<T>>::insert(
+				n_index.clone(),
+				Review {
+					user_id: who.clone(),
+					content: review_meta.clone(),
+					project_id: project_id.clone(),
+					proposal_status: Default::default(),
+				},
+			);
+			<ReviewIndex<T>>::put(new);
+			// update the project
+			<Projects<T>>::insert(project_id, this_project);
+			Self::deposit_event(Event::ReviewCreated(who, project_id));
 			Ok(())
 		}
 
@@ -260,18 +312,18 @@ pub mod pallet {
 	}
 	/// A separate impl pallet<T> for custom functions external to callables
 	impl<T: Config> Pallet<T> {
-		/// Parameters: owner_name: This is an &str converted to_Vec()
-		/// The owner_name will be decoded to utf-8 in the body for manipulation to derive metadata.
-		/// Eventaully refactor to ipfs storage.
+		/// Create a project from required data
 		pub fn initialize_projects(
 			this_owner_id: T::AccountId,
 			this_meta: Vec<u8>,
 			this_revs: Vec<ReviewID>,
+			this_reviewers: Vec<T::AccountId>,
 			this_status: Status,
 			this_reason: Reason,
 		) -> ProjectAl<T> {
 			let returnable = Project {
 				owner_id: this_owner_id,
+				reviewers: Option::Some(this_reviewers),
 				reviews: Option::Some(this_revs),
 				badge: Option::None,
 				metadata: this_meta,
@@ -282,14 +334,15 @@ pub mod pallet {
 		}
 		pub fn initialize_reviews(acnt_ids: Vec<T::AccountId>) -> Vec<ReviewID> {
 			let clns = acnt_ids.iter().clone();
-			let mut last_index = <ReviewIndex<T>>::get().unwrap_or_default();
-			let last_prj = <ProjectIndex<T>>::get().unwrap_or_default();
-			let list_of_revs: Vec<ReviewAl<T>> = project::REVS
+			let mut n_index = <ReviewIndex<T>>::get().unwrap_or_default();
+			let n_proj = <ProjectIndex<T>>::get().unwrap_or_default();
+			// intialize review contents with their ids
+			let list_of_revs: Vec<ReviewAl<T>> = constants::project::REVS
 				.iter()
 				.clone()
 				.zip(clns)
 				.map(|(rev, id)| Review {
-					project_id: last_prj,
+					project_id: n_proj,
 					proposal_status: ProposalStatus {
 						status: Status::Accepted,
 						reason: Default::default(),
@@ -298,23 +351,23 @@ pub mod pallet {
 					user_id: id.clone(),
 				})
 				.collect();
+			// storage mutations
 			let mut list_of_indexes: Vec<ReviewID> = Vec::new();
 			for elem in list_of_revs.iter() {
-				// shouldn't panic because we aren't placingg more than four in.
-				<Reviews<T>>::insert(last_index, elem);
-				list_of_indexes.push(last_index.clone());
-				last_index += 1;
+				// shouldn't panic because we aren't placing more than four in.
+				<Reviews<T>>::insert(n_index, elem);
+				list_of_indexes.push(n_index.clone());
+				n_index += 1;
 			}
-			<ReviewIndex<T>>::put(last_index);
+			<ReviewIndex<T>>::put(n_index);
 			return list_of_indexes;
 		}
 	}
 	/// Genesis config for the chocolate pallet
 	#[pallet::genesis_config]
 	pub struct GenesisConfig<T: Config> {
-		/// GEt the parameters for the init projects function
+		/// Get the parameters for the init projects function
 		pub init_projects: Vec<(T::AccountId, Status, Reason)>,
-		// There will be another entry for reviews - create only if prev passed.
 	}
 	/// By default a generic project or known projects will be shown - polkadot & sisters
 	#[cfg(feature = "std")]
@@ -330,24 +383,33 @@ pub mod pallet {
 		fn build(&self) {
 			// setup a counter to serve as project index
 			let mut count: ProjectID = 0;
-			// get the projects and insert to storage with name
-			let meta: Vec<Vec<u8>> = project::METADATA.iter().map(|each| each.to_vec()).collect();
+			let meta: Vec<Vec<u8>> =
+				constants::project::METADATA.iter().map(|each| each.to_vec()).collect();
 			let zipped = (&self.init_projects).into_iter().clone().zip(meta.iter().clone());
+			// create project from associated metadata in zip.
 			for each in zipped {
-				let (prj, meta_ref) = each.to_owned();
+				let (this_project, meta_ref) = each.to_owned();
 				let meta_cid = meta_ref.to_owned();
-				let (acnt, stat, reas) = prj.to_owned();
+				let (acnt, stat, reas) = this_project.to_owned();
+				// Filter ids so generated reviews do not include project owner
 				let filtered_ids: Vec<T::AccountId> = (&self.init_projects)
 					.into_iter()
 					.clone()
 					.filter(|(id, ..)| acnt.ne(id))
 					.map(|long| long.0.clone())
 					.collect();
-				let review_ids: Vec<ReviewID> = Pallet::<T>::initialize_reviews(filtered_ids);
-				let returnable =
-					Pallet::<T>::initialize_projects(acnt, meta_cid, review_ids, stat, reas);
-				let ret_count = count;
-				<Projects<T>>::insert(ret_count, returnable);
+				// create reviews and projects and store.
+				let review_ids: Vec<ReviewID> =
+					Pallet::<T>::initialize_reviews(filtered_ids.clone());
+				let returnable = Pallet::<T>::initialize_projects(
+					acnt,
+					meta_cid,
+					review_ids,
+					filtered_ids,
+					stat,
+					reas,
+				);
+				<Projects<T>>::insert(count.clone(), returnable);
 				count += 1;
 				<ProjectIndex<T>>::put(count);
 			}
