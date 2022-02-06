f3a75d304b90fad80085844051590411f2457101
SHA888 <kresnasucandra@gmail.com>
2022-02-06 21:59:48 +0800

    Add create review test

--- pallets/chocolate/src/tests.rs
+++ pallets/chocolate/src/tests.rs
@@ -31,20 +31,20 @@ fn create_project_should_work() {
 fn create_project_should_fail() {
 	new_test_ext().execute_with(|| {
 		assert_ok!(ChocolateModule::create_project(Origin::signed(1), [40_u8].to_vec()));
-	})
+	});
 }
 
-// #[test]
-// fn create_review_should_work() {
-// 	new_test_ext().execute_with(|| {
-// 		// Dispatch a signed extrinsic.
-// 		assert_ok!(ChocolateModule::create_review(Origin::signed(1), [42_u8].to_vec(), 42_u32));
-// 	});
-// }
+#[test]
+fn create_review_should_work() {
+	new_test_ext().execute_with(|| {
+		// Dispatch a signed extrinsic.
+		assert_ok!(ChocolateModule::create_review(Origin::signed(1), [42_u8].to_vec(), 42_u32));
+	});
+}
 
-// fn create_review_should_fail() {
-// 	new_test_ext().execute_with(|| {
-// 		// Dispatch a signed extrinsic.
-// 		assert_ok!(ChocolateModule::create_review(Origin::signed(1), [40_u8].to_vec(), 42_u32));
-// 	});
-// }
+fn create_review_should_fail() {
+	new_test_ext().execute_with(|| {
+		// Dispatch a signed extrinsic.
+		assert_ok!(ChocolateModule::create_review(Origin::signed(1), [40_u8].to_vec(), 40_u32));
+	});
+}
