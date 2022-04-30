fdad00056fa0f5187a097b79415800c992e6254b
Islam <islamkwnd@gmail.com>
2022-04-30 09:19:30 +0000

    (feat) Included review score onchain.
    * Added review score in review struct
    * Added number of reviews and sum of score for easy average on project
    * Cleaned up genesis config to depend on extrinsics
    * Removed expect(s) of collateralise to return err instead.
    * Increase user rank point by one in reward_user
    * Reordered mutations in a way that the least likely to err are called last.
    * Added more tests to chocolate pallet
    
    **Review rewarding logic for a more graceful way of handling rewarding
    * ToDo: Move docs on projectIO impl to trait definition.

--- primitives/chocolate-users/src/lib.rs
+++ primitives/chocolate-users/src/lib.rs
@@ -4,19 +4,26 @@ use codec::{Decode, Encode};
 use frame_support::dispatch::{ DispatchResult};
 use frame_system::Config;
 
-#[derive(Encode, Decode, Default, Clone)]
+#[derive(Encode, Decode, Clone)]
 pub struct User {
 	pub rank_points: u32,
 	pub project_id: Option<u32>,
 }
-
+impl Default for User{
+	fn default() -> Self{
+		// Start from 1 because of total project score calc to avoid accidentally recording zero when we use Default::default()
+		User { rank_points: 1, project_id: Option::None }
+	}
+}
 /// UserIO trait for CRUD on users store
 pub trait UserIO<T: Config> {
 	fn get_user_by_id(id: &T::AccountId) -> Option<User>;
 	fn check_owns_project(id: &T::AccountId) -> bool;
+	/// Allows us to check if the user even exists before calling get by id.
 	fn check_user_exists(id: &T::AccountId) -> bool;
 	/// Checks if the user exists, else creates a new user with wanted defaults.
 	fn get_or_create_default(id: &T::AccountId) -> User;
+	/// Idempotent. Simply creates item in storage if it  doesn't already exist. Use update_user if you'd like to mutate the user after knowing it's been created
 	fn set_user(id: &T::AccountId, user: User) -> ();
 	fn update_user(id: &T::AccountId, user: User) -> DispatchResult;
 }
