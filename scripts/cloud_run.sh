03b10486f8aa2eb7ab7e5dab2cbe639aae9f2701
Islam <islamkwnd@gmail.com>
2022-05-15 08:19:08 +0000

    release: New image hash

--- scripts/cloud_run.sh
+++ scripts/cloud_run.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/bash
+set -euo pipefail
 
-# Run docker build with tag
-TAG=islami00/choc-test
-docker build -f .github/dockerfiles/Dockerfile.ubuntu-base -t $TAG .
+docker login
+DATE=$(date +"%d%m%Y")
+TAGGEDREPO="islami00/choc-test:$DATE"
+
+cargo build --release
+docker build -f .github/dockerfiles/Dockerfile.ubuntu-base -t $TAGGEDREPO .
 #   give option for USER and GROUP
-# Grab dockerimage hash.
-TAGGED_HASH=$(docker inspect --format='{{index .RepoDigests 0}}' $TAG) 
-#   Combine image hash with name
-SED_ME="s+%%DOCKER_IMAGE%%+${TAGGED_HASH}+g"
-#   Replace DOCKER_IMAGE in docker-compose.yml.template
+SED_ME="s+%%DOCKER_IMAGE%%+$TAGGEDREPO+g"
+
 cat .github/dockerfiles/docker-compose.yml.template | sed $SED_ME  > docker-compose.yml &&
-#   Make commit with tag release
+
 git add docker-compose.yml && git commit -m "release: New image hash"
-# Push to repo
-docker push $TAG
+
+docker push $TAGGEDREPO
